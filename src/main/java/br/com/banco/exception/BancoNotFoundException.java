1757ce4fa7299c3b853883b07f7dc3089a8764d9
thiago.batista <wesleyaz0909@gmail.com>
2023-08-02 01:36:05 -0300

    Criacao e evolucao das pesquisas

--- src/main/java/br/com/banco/exception/BancoNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package br.com.banco.exception;
-
-public class BancoNotFoundException extends RuntimeException {
-    public BancoNotFoundException(final String message) {
-        super(message);
-    }
-}
--- src/main/java/br/com/banco/response/mapper/TransferResponseMapper.java
+++
@@ -1,4 +0,0 @@
-package br.com.banco.response.mapper;
-
-public interface TransferResponseMapper {
-}
